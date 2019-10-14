cf14b72fa8677f509a169a0b49feefdf0a06f8a6
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-14 23:48:02 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,24 +1,4 @@
 
-def hopper
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-
-end
-
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
@@ -35,9 +15,11 @@ def alan_kay_is_known_for
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+         }
      }
+    programmer_hash[:grace_hopper]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
