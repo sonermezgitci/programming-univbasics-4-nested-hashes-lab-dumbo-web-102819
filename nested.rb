8eb2cc7f6ecf93157c42c2c2888b8dea72c27d21
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-15 16:51:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ def changing_alan
       languages: ["C"] 
         }
      }
-   
+   changing_alan[:alan_kay][:known_for]= "GUI"
     
 end
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
